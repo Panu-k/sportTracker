2ac11e2f9daab06fbaf63e51c6a68de69d159520
Panu Kiminki <Panu.Kiminki@edu.savonia.fi>
2022-04-12 23:59:38 +0300

    Tiedon välitys onnistuu Backendistä

--- sport_tracker/lib/login_page.dart
+++ sport_tracker/lib/login_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:sport_tracker/main.dart';
+import 'package:http/http.dart' as http;
+import 'package:sport_tracker/user.dart';
 
 // ignore: use_key_in_widget_constructors
 class Loginpage extends StatefulWidget {
@@ -13,6 +16,22 @@ class _LoginpageState extends State<Loginpage> {
   final _usernameController = TextEditingController();
 
   final _passwordController = TextEditingController();
+
+  getUserData() async {
+    var response = await http.get(Uri.http('10.0.2.2:3002', 'users'));
+    var jsondata = jsonDecode(response.body);
+
+    print(jsondata.toString());
+    List<User> users = [];
+
+    for (var u in jsondata) {
+      User user = User(u["idUsers"], u["Name"], u["Email"], u["UserName"]);
+      users.add(user);
+    }
+    print(users[0].name);
+
+    return users;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,46 +50,45 @@ class _LoginpageState extends State<Loginpage> {
                 child: SingleChildScrollView(
                   child: Form(
                       key: formKey,
-                      child: Center(
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                TextFormField(
-                                  controller: _usernameController,
-                                  keyboardType: TextInputType.emailAddress,
-                                  decoration: const InputDecoration(
-                                      hintText: "Enter Username",
-                                      labelText: "Username"),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                TextFormField(
-                                  controller: _passwordController,
-                                  keyboardType: TextInputType.text,
-                                  obscureText: true,
-                                  decoration: const InputDecoration(
-                                      hintText: "Enter Password",
-                                      labelText: "Password"),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const MyHomePage(
-                                                      title: "Flutter Demo")));
-                                    },
-                                    child: Text("Sing in"))
-                              ],
-                            ),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              TextFormField(
+                                controller: _usernameController,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: const InputDecoration(
+                                    hintText: "Enter Username",
+                                    labelText: "Username"),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              TextFormField(
+                                controller: _passwordController,
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                    hintText: "Enter Password",
+                                    labelText: "Password"),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    getUserData();
+                                    //Navigator.push(
+                                    //  context,
+                                    // MaterialPageRoute(
+                                    //    builder: (context) =>
+                                    //       const MyHomePage(
+                                    //          title: "Flutter Demo")));
+                                  },
+                                  child: Text("Sing in"))
+                            ],
                           ),
                         ),
                       )),
---
+++ sport_tracker/lib/user.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class User extends StatelessWidget {
+  String username;
+  String email;
+  String name;
+  int idUser;
+
+  User(this.idUser, this.name, this.email, this.username);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    throw UnimplementedError();
+  }
+}
